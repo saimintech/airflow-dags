a18f4990b125b30a4cf39053a228544c9f8e8252
Saim Safdar <53660940+saimintech@users.noreply.github.com>
2024-09-06 12:46:24 +0500

    Update dag_weather.py

--- Weather-data-to-Sheets/dag_weather.py
+++ Weather-data-to-Sheets/dag_weather.py
@@ -6,7 +6,7 @@ from weather_utils import get_current_weather
 from airflow.models import Variable
 
 WEATHER_API = Variable.get('WEATHER_API_KEY_SECRET')
-
+WEATHER_LOCATION = Variable.get('WEATHER_API_LOCATION')
 
 default_args = {
     "start_date":datetime(2023, 8, 11),
@@ -19,5 +19,5 @@ with DAG('dag_weather', schedule_interval='59 16 * * *', default_args=default_ar
     retrieve_data = PythonOperator(
         task_id = 'task_id_1',
         python_callable = get_current_weather,
-        op_args = [WEATHER_API]
+        op_args = [WEATHER_API, WEATHER_LOCATION]
     )
